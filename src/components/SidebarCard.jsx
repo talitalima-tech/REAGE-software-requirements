ef914501de0334b82d99f8e0f5c15cb654bcf543
Talita Lima <talita.lima.tech@gmail.com>
2026-09-09 13:56:20 -0300

    feat: alteração no sidebarcard

--- src/components/SidebarCard.jsx
+++ src/components/SidebarCard.jsx
@@ -1,4 +1,6 @@
 // src/components/SidebarCard.jsx
+import React from 'react';
+
 export const SidebarCard = ({ title, children }) => (
   <div className="border-2 border-sky-200 rounded-xl p-4 bg-white relative mt-6">
     <h3 className="absolute -top-3 left-1/2 -translate-x-1/2 bg-white px-4 font-bold text-reage-dark text-sm whitespace-nowrap">
